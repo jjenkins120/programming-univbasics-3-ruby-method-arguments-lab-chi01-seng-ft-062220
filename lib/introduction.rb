018ebfc2c1e60942c8717eba5f0e53e459168550
Jeff Jenkins <jjenkins120@gmail.com>
2020-05-14 17:15:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,7 +9,17 @@ end
 
 introduction("Jeff")
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
+introduction_with_language("Jeff", "Ruby")
+
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional("Jeff")
 
 
 
